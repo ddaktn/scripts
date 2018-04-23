7d49a99212373c858fd9e652665e0346acf7826c
Nelson <mss92473@corp.mutualofomaha.com>
2018-04-23 16:57:14 -0500

    removed FindComputersWithOU.ps1

--- AD_GPOs/FindComputersWithOU.ps1
+++
@@ -1,3 +0,0 @@
-﻿$ADComputers = get-adcomputer -filter * -SearchBase "DC=corp,DC=mutualofomaha,DC=com" -Properties CanonicalName,DistinguishedName, CN
-
-$ComputerLocation = $ADComputers | select CN, @{Name='OU'; Expression={$_.CanonicalName.Replace('corp.mutualofomaha.com/','').Replace("/$($_.Name)",'')}}
