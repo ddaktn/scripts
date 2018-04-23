cfff34d36aff9874512cc435f96b7cf179c62c2b
Nelson <mss92473@corp.mutualofomaha.com>
2018-04-23 16:58:44 -0500

    removed FindRecursiveGroupMembers.ps1

--- AD_GPOs/FindRecursiveGroupMembers.ps1
+++
@@ -1,3 +0,0 @@
-﻿#Recursively get group membership by using get-aduser and distinguished name of group
-
-Get-ADUser -Filter {memberOf -recursivematch 'cn=Domain Admins,cn=Users,dc=BCBSNEPRD,dc=com'}
