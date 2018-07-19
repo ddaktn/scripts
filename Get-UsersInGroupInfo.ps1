83fa2a65da2a99f938350b582c0f160a79c112a7
Nelson <mss92473@corp.mutualofomaha.com>
2018-07-18 22:15:29 -0500

    added script to get group member user info

---
+++ Get-UsersInGroupInfo.ps1
@@ -0,0 +1,15 @@
+﻿$groups = Get-ADGroup -Filter {name -like "VDI-Pool*"}
+
+$output = ForEach ($g in $groups){
+    $results = Get-ADGroupMember -Identity $g.name -Recursive | Get-ADUser -Properties Name, Description, Office, Manager
+    ForEach ($r in $results){
+        New-Object PSObject -Property @{
+            VDIPool = $g.Name
+            User = $r.SamAccountName
+            Description = $r.Description
+            Office = $r.Office
+            Manager = ( get-aduser ( get-aduser $r -Property manager ).manager).samaccountname        
+        }
+    }
+} 
+ $output | Export-Csv -path c:\temp\output.csv
