9a705bcf7138d582ebd93be1d16561fbf0f5a312
Nelson <mss92473@corp.mutualofomaha.com>
2018-07-24 15:22:56 -0500

    7/24/2018

--- CheckPortOpenClosed.ps1
+++ CheckPortOpenClosed.ps1
@@ -1,35 +1,49 @@
-﻿### Script to check TCP and UDP port connectivity ###
+﻿<#
+    Script to check TCP and UDP port connectivity
+    Doug Nelson
+    7/24/2018
+#>
 
-## Create a variable for the remote machine to check connectivity to ##
-Write-Host "Enter IP or hostname for port check:" -ForegroundColor Magenta
-$RemoteMachine = Read-Host
+[CmdletBinding()]
+    PARAM(
+        [Parameter(Mandatory=$false,
+                   Position=0)]
+        [string]$RemoteMachine,
 
-## Create variable for the port to check connectivity with ##
-Write-Host "Enter port to check:" -ForegroundColor Magenta
-$Port= Read-host
-
-## Create TCP object to check connections ##
-$t = New-Object Net.Sockets.TcpClient
-
-## Create UDP object to check connections ##
-$u = New-Object Net.Sockets.UdpClient
-
-## Script block to check TCP connectivity ##
-$t.Connect($RemoteMachine,$Port)
-    if($t.Connected)
-    {
-        Write-Host "TCP port $Port is operational" -ForegroundColor Green
-    }else
-    {
-        Write-Host "!!!TCP port $Port is CLOSED!!!" -ForegroundColor Red
+        [Parameter(Mandatory=$false,
+                   Position=1)]
+        [string]$Port
+    )
+FUNCTION Check-PortConnect{
+    BEGIN{
+        $ErrorActionPreference = 'SilentlyContinue'
+        if($RemoteMachine -eq ""){
+            Write-Host "Enter IP or hostname for port check:" -ForegroundColor Magenta
+            $RemoteMachine = Read-Host
+        }
+        if($Port -eq ""){
+            Write-Host "Enter port to check:" -ForegroundColor Magenta
+            $Port= Read-host
+        }
+        $t = New-Object Net.Sockets.TcpClient
+        $u = New-Object Net.Sockets.UdpClient
     }
-
-## Script block to check UDP connectivity ##
-$u.Connect($RemoteMachine,$Port)
-    if($u.Connected)
-    {
-        Write-Host "UDP port $Port is operational" -ForegroundColor Green
-    }else
-    {
-        Write-Host "!!!UDP port $Port is CLOSED!!!" -ForegroundColor Red
+    PROCESS{
+        ## Script block to check TCP connectivity ##
+        $t.Connect($RemoteMachine,$Port)
+            if($t.Connected){
+                Write-Host "TCP port $Port is operational" -ForegroundColor Green
+            } else {
+                Write-Host "!!!TCP port $Port is CLOSED!!!" -ForegroundColor Red
+            }
+        ## Script block to check UDP connectivity ##
+        $u.Connect($RemoteMachine,$Port)
+            if($u.Connected){
+                Write-Host "UDP port $Port is operational" -ForegroundColor Green
+            } else {
+                Write-Host "!!!UDP port $Port is CLOSED!!!" -ForegroundColor Red
+            }
     }
+    END{}
+}
+Check-PortConnect $RemoteMachine $Port
