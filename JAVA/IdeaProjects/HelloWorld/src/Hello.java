89473c0471e627f50c1fe90dad2913344e80c8ff
Nelson <mss92473@corp.mutualofomaha.com>
2018-09-13 16:21:45 -0500

    SEPT 13 2018

--- JAVA/IdeaProjects/HelloWorld/src/Hello.java
+++ JAVA/IdeaProjects/HelloWorld/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         String name = "World";
-        System.out.println("Hello " + name + "!");
+        System.out.println("Hello "+name+"!");
         int myFirstNumber = (10 + 5) + (2 * 10);
         System.out.println(myFirstNumber);
     }
