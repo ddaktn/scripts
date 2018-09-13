1bad8ae596e70573f1c09c274b8f5eed63478b78
Nelson <mss92473@corp.mutualofomaha.com>
2018-09-13 16:07:57 -0500

    added java projects

---
+++ JAVA/IdeaProjects/HelloWorld/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        String name = "World";
+        System.out.println("Hello " + name + "!");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+    }
+}
---
+++ JAVA/NetBeansProjects/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mss92473
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "World";
+        System.out.println("Hello " + name + "!");
+    }
+    
+}
