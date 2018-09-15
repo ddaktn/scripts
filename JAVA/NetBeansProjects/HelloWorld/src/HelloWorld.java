9a619db33f0cdad8e3c21dde61728eb8d4abb0cc
Nelson <mss92473@corp.mutualofomaha.com>
2018-09-15 09:21:16 -0500

    Added java, python, and js scripts

--- JAVA/NetBeansProjects/HelloWorld/src/HelloWorld.java
+++ JAVA/NetBeansProjects/HelloWorld/src/HelloWorld.java
@@ -14,7 +14,6 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         String name = "World";
         System.out.println("Hello " + name + "!");
     }
