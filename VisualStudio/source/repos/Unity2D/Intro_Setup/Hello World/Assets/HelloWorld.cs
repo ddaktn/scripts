e6ba1cb32d93043d522605c7a9c7886b97013ac8
Nelson <mss92473@corp.mutualofomaha.com>
2018-11-19 13:58:01 -0600

    11/19/2018

---
+++ VisualStudio/source/repos/Unity2D/Intro_Setup/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initializations
+	void Start () {
+        string name = "Doug";
+        print("Hello " + name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
