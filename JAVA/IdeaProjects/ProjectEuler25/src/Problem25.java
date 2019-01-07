9c55b5faf0e06c11160471254922c43ad4d385d4
Nelson <mss92473@corp.mutualofomaha.com>
2019-01-07 16:08:32 -0600

    01/07/2019

--- JAVA/IdeaProjects/ProjectEuler25/src/Problem25.java
+++ JAVA/IdeaProjects/ProjectEuler25/src/Problem25.java
@@ -1,13 +1,21 @@
+import java.util.Arrays;
+
 public class Problem25 {
 
     public static void main(String[] args){
         //1000-digit Fibonacci number
-        int[] intArr = new int[2];
+        int[] intArr = new int[12];
         intArr[0] = 1;
         intArr[1] = 1;
-        for(int i = 2; i < 100; i++){
-            if(){
-
+        System.out.println(intArr[0]);
+        System.out.println(intArr[1]);
+        for(int i = 2; i < intArr.length; i++){
+            intArr = Arrays.copyOf(intArr, intArr.length + 1);
+            intArr[i] = intArr[i - 1] + intArr[i -2];
+            System.out.println(intArr[i]);
+            int length = String.valueOf(intArr[i]).length();
+            if(length / 1000 == 1) {
+                break;
             }
         }
     }
